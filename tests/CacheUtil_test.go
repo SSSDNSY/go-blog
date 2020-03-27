8dcce5608e0e5ce49dee4ec8bb04fb8fea1e5d85
pengzh <pengzh@asiainfo.com>
2020-03-27 16:23:59 +0800

    【新增】Markdown编辑器

---
+++ tests/CacheUtil_test.go
@@ -0,0 +1,27 @@
+package test
+
+import (
+	"fmt"
+	"go-blog/util"
+	"testing"
+)
+
+func TestCache(t *testing.T) {
+
+	c := util.GetIns()
+	c.PutVal("int", 1)
+	c.PutVal("string", "234")
+	c.PutVal("arr", []int{1, 34, 3})
+
+	m1 := make(map[string]string)
+	m1["k1"] = "val1"
+	m1["k2"] = "val2"
+	c.PutVal("map", m1)
+	fmt.Println(c)
+	fmt.Println(c.Empty())
+	fmt.Println(c.Size())
+	fmt.Println(c.GetVal("map"))
+	fmt.Println(c.GetVal("int"))
+	fmt.Printf("类型=%T ,值=%d\n", c.GetVal("int"), c.GetVal("int"))
+	fmt.Printf("类型=%T ,值=%+v\n", c.GetVal("map"), c.GetVal("map"))
+}
---
+++ util/CacheUtil.go
@@ -0,0 +1,65 @@
+package util
+
+import "strings"
+
+type CachMap struct {
+	Cache map[string]interface{}
+}
+
+//在这里接口怎么用啊
+type Cacher interface {
+	Contain(key string) bool
+	Empty() bool
+	GetVal(key string) interface{}
+	PutVal(key string, val interface{}) bool
+	Size() int
+}
+
+func GetIns() *CachMap {
+	CachMap := new(CachMap)
+	CachMap.Cache = make(map[string]interface{}, 1000)
+	return CachMap
+}
+func (c *CachMap) Size() int {
+
+	if c.Cache != nil {
+		return len(c.Cache)
+	} else {
+		return -1
+	}
+}
+
+func (c *CachMap) Contain(key string) bool {
+	val, ok := c.Cache[key]
+	if ok && val != nil {
+		return true
+	} else {
+		return false
+	}
+}
+
+func (c *CachMap) Empty() bool {
+	if len(c.Cache) > 0 {
+		return false
+	} else {
+		return true
+	}
+}
+
+func (c *CachMap) GetVal(key string) interface{} {
+	val, ok := c.Cache[key]
+	if ok {
+		return val
+	} else {
+		return ok
+	}
+}
+
+func (c *CachMap) PutVal(key string, val interface{}) interface{} {
+	if len(strings.TrimSpace(key)) == 0 {
+		return false
+	} else {
+		c.Cache[key] = val
+	}
+	return true
+}
